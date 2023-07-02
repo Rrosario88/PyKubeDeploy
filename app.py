7044902c48946535be0368e6c73a1bcf7cb5f4dc
rorosario <rorosario@me.com>
2023-07-01 21:15:01 -0400

    creating python3 app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import redis
+
+app = Flask(__name__)
+cache = redis.Redis(host='localhost', port=6379, db=0)
+
+@app.route('/')
+def hello():
+    count = cache.incr('hits')
+    return 'Hello World! This page has been viewed {} times.\n'.format(count)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
