ba9f01a9c54c4035b110c694d9cf40c3ab5c95f4
rorosario <rorosario@me.com>
2023-07-03 22:21:14 -0400

    update

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "ec2_instance" "pkd_ec2" {
+  ami           = "ami-0c94855ba95c574c8"
+  instance_type = "t2.micro"
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install -y docker
+              sudo service docker start
+              sudo usermod -a -G docker ec2-user
+              EOF
+}
