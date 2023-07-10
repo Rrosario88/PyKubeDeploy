825455831263a557c8c2323dce7e109cba5266ce
rorosario <rorosario@me.com>
2023-07-10 02:05:50 -0400

    adding vpc

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "pkd_ec2" {
-   ami           = "ami-00abfd8da690f984d"
+  ami           = "ami-00abfd8da690f984d"
   instance_type = "t2.micro"
-  
+  vpc_id        = "vpc-0745da24b1c8cae53" 
+
   user_data = <<-EOF
               #!/bin/bash
               sudo yum update -y
