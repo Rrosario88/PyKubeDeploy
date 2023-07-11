5d78e8b894d4dfc9a09eb2e6f4e47290de26548c
rorosario <rorosario@me.com>
2023-07-10 23:47:12 -0400

    fix

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "pkd_ec2" {
   ami           = "ami-09cd431658b5ab3be"
   instance_type = "t2.micro"
   subnet_id     = "subnet-0ed2ed10a337623af"
-  key_name   = "PyKubeKeys.pem"
+  key_name   = "PyKubeKeys"
 
   user_data = <<-EOF
               #!/bin/bash
@@ -12,12 +12,14 @@ resource "aws_instance" "pkd_ec2" {
               sudo usermod -a -G docker ec2-user
               EOF
 
-output "instance_ip" {
-  value = aws_instance.my_instance.public_ip
-}
-
+  provisioner "local-exec" {
+    command = "${path.module}/update_inventory.sh"
 
   tags = {
     name = "PyKubeDeploy"
   }            
+}
+
+output "instance_ip" {
+  value = aws_instance.pkd_ec2.public_ip
 }
