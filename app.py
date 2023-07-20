78d2fd168e8e5bb6bf5ad5ca1bbb2bf68266221d
rorosario <rorosario@me.com>
2023-07-19 21:59:07 -0400

    update

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ cache = Redis(host=os.getenv('REDIS_HOST', 'localhost'), port=6379, db=0)
 @app.route('/')
 def hello():
     count = cache.incr('hits')
-    return 'Hello World! This amazing page has been viewed {} times.\n'.format(count)
+    return 'Welcome to my project Alexis ;) This amazing page has been viewed {} times.\n'.format(count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
