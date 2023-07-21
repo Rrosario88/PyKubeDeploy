4025ae148c53879db7fc93dfaf5eac8240d044db
rorosario <rorosario@me.com>
2023-07-21 01:26:15 -0400

    switching to free tier ami

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "pkd_ec2" {
-  ami           = "ami-09cd431658b5ab3be"
+  ami           = "ami-05548f9cecf47b442"
   instance_type = "t2.micro"
   subnet_id     = "subnet-0ed2ed10a337623af"
   key_name      = "PyKubeKeys"
