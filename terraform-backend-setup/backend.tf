b6626f93e7f05dd0c772c11c373d4785a067b38b
rorosario <rorosario@me.com>
2023-10-07 13:06:54 -0400

    adding backend

---
+++ terraform-backend-setup/backend.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1" 
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "tfstates" 
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name           = "terraform-up-and-running-locks"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
