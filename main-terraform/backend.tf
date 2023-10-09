6d53ce46c6de2f3ba3d82f29cca77c4b0f09c1a8
rorosario <rorosario@me.com>
2023-10-08 20:51:33 -0400

    update

--- main-terraform/backend.tf
+++ main-terraform/backend.tf
@@ -19,7 +19,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket         = "tform-states" 
-    key            = "~/.ssh/CastleKeys.pub"
+    key            = "PyKubeDeploy/terraform.tfstate"
     region         = "us-east-1" 
     dynamodb_table = "terraform-up-and-running-locks"
     encrypt        = true
