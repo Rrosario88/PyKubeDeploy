a63ff683a43c3535f1f115503c5cc1d619879aad
rorosario <rorosario@me.com>
2024-07-04 18:09:37 -0400

    secrets

--- main-terraform/backend.tf
+++ main-terraform/backend.tf
@@ -8,13 +8,27 @@ terraform {
   }
 }
 
+# Configure the AWS provider with temporary static credentials
 provider "aws" {
-
+  alias      = "static"
+  access_key = "TEMPORARY_ACCESS_KEY"
+  secret_key = "TEMPORARY_SECRET_KEY"
   region     = "us-east-1"
-
-  access_key = jsondecode(data.aws_secretsmanager_secret_version.my_credentials_version.secret_string)["aws_access_key_id"]
-  secret_key = jsondecode(data.aws_secretsmanager_secret_version.my_credentials_version.secret_string)["aws_secret_access_key"]
 }
+
+# Retrieve the AWS credentials from Secrets Manager
+data "aws_secretsmanager_secret_value" "my_credentials" {
+  provider  = aws.static
+  secret_id = "AWSCastleKeys"
+}
+
+# Configure the AWS provider with the retrieved credentials
+provider "aws" {
+  access_key = jsondecode(data.aws_secretsmanager_secret_value.my_credentials.secret_string)["aws_access_key_id"]
+  secret_key = jsondecode(data.aws_secretsmanager_secret_value.my_credentials.secret_string)["aws_secret_access_key"]
+  region     = "us-east-1"
+}
+
 
 terraform {
   backend "s3" {
--- main-terraform/variables.tf
+++ main-terraform/variables.tf
@@ -9,11 +9,5 @@ variable "AWS_SECRET_ACCESS_KEY" {
 }
 */
 # Retrieve the AWS credentials from Secrets Manager
-data "aws_secretsmanager_secret" "my_credentials" {
-  name = "AWSCastleKeys"
-}
 
-data "aws_secretsmanager_secret_version" "my_credentials_version" {
-  secret_id = data.aws_secretsmanager_secret.my_credentials.id
-  version_id = data.aws_secretsmanager_secret.my_credentials.version_id
-}
+
